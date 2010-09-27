e9a0be6d54da62a86dc9d9efe753a0ed7bc01e18
ville.krumlinde@gmail.com <ville@emix8.org>
2010-09-27 08:58:55 +0000

    Anchor Modeler Google Cloud Engine CodeList/load methods initiated. Still problems with cross-site scripting.

--- Anchormodeler/src/anchormodeler/AnchormodelerServlet.java
+++ Anchormodeler/src/anchormodeler/AnchormodelerServlet.java
@@ -3,14 +3,15 @@ package anchormodeler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.FileItemIterator;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 
@@ -26,7 +27,8 @@ public class AnchormodelerServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.setContentType("text/plain");
+		//resp.setContentType("text/html");
+		
 
 		ServletFileUpload upload = new ServletFileUpload();
 		AnchorRequest areq = new AnchorRequest();
@@ -42,21 +44,25 @@ public class AnchormodelerServlet extends HttpServlet {
 					areq.fileParams.put(item.getFieldName(),item);
 				}
 			}
-
-		} catch (FileUploadException e) {
+		
+			String action = areq.stringParams.get("action");
+			action=(action == null) ? "" : action;
+			action=action.toLowerCase();
+			
+			if (action.equals("save")) {
+				actionSave(areq, resp); 
+			} else if (action.equals("load")) {
+				actionLoad(areq, resp);
+			} else if (action.equals("list")) {
+				actionList(areq, resp);
+			} else {
+				resp.getWriter().println("ERROR: unknown action");
+			}
+			
+		} catch (Exception e) {
 			resp.getWriter().println("ERROR");
 			resp.getWriter().println( e.toString() );
 			return;
-		}
-		
-		String action = areq.stringParams.get("action");
-		action=(action == null) ? "" : action;
-		action=action.toLowerCase();
-		
-		if (action.equals("save")) {
-			actionSave(areq, resp);
-		} else {
-			resp.getWriter().println("ERROR: unknown action");
 		}
 	}
 
@@ -78,11 +84,14 @@ public class AnchormodelerServlet extends HttpServlet {
 	private void actionSave(AnchorRequest areq, HttpServletResponse resp) throws ServletException, IOException {
 		/*
 		 * scope "public","private" modelName modelId (om nytt dokument så
-		 * slopar man detta, annars overwrite model med detta id) keywords icon
-		 * (textencoded png-image) modelXml
+		 * slopar man detta, annars overwrite model med detta id) 
+		 * keywords 
+		 * icon (textencoded png-image)
+		 * modelXml
 		 * 
 		 * returnerar ok/error
 		 */
+		//TODO: more data to save
 		String modelName = areq.stringParams.get("modelName");
 		String modelXml = areq.stringParams.get("modelXml");
 
@@ -98,4 +107,52 @@ public class AnchormodelerServlet extends HttpServlet {
 		resp.getWriter().println("OK");
 	}
 
+	private void actionLoad(AnchorRequest areq, HttpServletResponse resp) throws IOException {
+		String modelId = areq.stringParams.get("modelId");
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			Model m = (Model)pm.getObjectById(modelId);
+			resp.getWriter().println("OK");
+			resp.getWriter().println( m.getModelXml() );
+		} finally {
+			pm.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void actionList(AnchorRequest areq, HttpServletResponse resp) throws IOException {
+		/*list
+		  scope "public","private","public/private"
+		  filterBy "keyword"
+		  filterValue "lkslejg"
+		  maxItemsReturned 20
+		
+		  returnerar i xmlformat
+		    modelId, modelName, icon, domain, keyword, authorName
+		*/
+
+		//TODO: format as xml http://www.roseindia.net/servlets/xm-servlet.shtml
+		
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Query query=null;
+	    try {
+		    query = pm.newQuery(Model.class);
+		    //query.setFilter("lastName == lastNameParam");
+		    //query.setOrdering("hireDate desc");
+		    //query.declareParameters("String lastNameParam");
+
+        	resp.getWriter().println("debug");
+	        List<Model> results = (List<Model>) query.execute();
+            for (Model m : results) {
+                // ...
+            	resp.getWriter().println(m.getModelName());
+            }
+	    } finally {
+	        if(query!=null)
+	        	query.closeAll();
+			pm.close();
+	    }		
+
+	}
+	
 }
