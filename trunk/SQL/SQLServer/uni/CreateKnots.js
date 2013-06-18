9d188540381b214b306145b61c32b2e4a629f182
lars.ronnback <lars.ronnback@bfa991c8-53a5-3350-3af9-35fd0b1b703c>
2013-06-18 06:35:11 +0000

    Very small change that fixed bug with metadata.

--- trunk/SQL/SQLServer/uni/CreateKnots.js
+++ trunk/SQL/SQLServer/uni/CreateKnots.js
@@ -2,18 +2,19 @@
 /*
  *  Knots are used to store finite sets of values, normally used to describe states
  *  of entities (through knotted attributes) or relationships (through knotted ties).
+ *
  */
 ~
-var knot, metadataDefinition;
+var knot;
 for(var k = 0; knot = schema.knot[schema.knots[k]]; k++) {
     if(knot.metadata.generator == 'true')
         knot.identityGenerator = 'IDENTITY(1,1)';
     else
         knot.identityGenerator = '';
     if(schema.metadataUsage == 'true')
-        metadataDefinition = knot.metadataColumnName + ' ' + $schema.metadataType + ' not null,';
+        knot.metadataDefinition = knot.metadataColumnName + ' ' + schema.metadataType + ' not null,';
     else
-        metadataDefinition = '';
+        knot.metadataDefinition = '';
 ~
 ----------------------------------- [Knot Table] -------------------------------------
 -- $knot.name table (number ${(knot.position + 1)}$ of $schema.knots.length)
@@ -22,7 +23,7 @@ IF NOT EXISTS (SELECT * FROM sys.objects WHERE name = $knot.name and type LIKE '
 CREATE TABLE [$knot.capsule].[$knot.name] (
     $knot.identityColumnName $knot.identity $knot.identityGenerator not null,
     $knot.name $knot.dataRange not null,
-    $metadataDefinition
+    $knot.metadataDefinition
     constraint pk$knot.name primary key (
         $knot.identityColumnName asc
     ),
