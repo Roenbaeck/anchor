8093de8f4513145f3e3e0d80f8384c6816c01d09
lars.ronnback@gmail.com <lars.ronnback@gmail.com@bfa991c8-53a5-3350-3af9-35fd0b1b703c>
2014-02-23 10:23:43 +0000

    Fixed  small bug in the Sisulator where the quoting was applied to early, but did not affect any existing templates. Started to add support for equivalence (work in progress).

---
+++ trunk/SQL/SQLServer/uni_eq/CreateAttributeRewinders.js
@@ -0,0 +1,72 @@
+/*~
+-- ATTRIBUTE REWINDERS ------------------------------------------------------------------------------------------------
+--
+-- These table valued functions rewind an attribute table to the given
+-- point in changing time. It does not pick a temporal perspective and
+-- instead shows all rows that have been in effect before that point
+-- in time.
+--
+-- @changingTimepoint   the point in changing time to rewind to
+--
+~*/
+var anchor;
+while (anchor = schema.nextAnchor()) {
+    var knot, attribute;
+    while (attribute = anchor.nextAttribute()) {
+        if(attribute.isHistorized()) {
+            if(!attribute.isKnotted()) {
+/*~
+-- Attribute rewinder -------------------------------------------------------------------------------------------------
+-- r$attribute.name rewinding over changing time function
+-----------------------------------------------------------------------------------------------------------------------
+IF Object_ID('r$attribute.name','IF') IS NULL
+BEGIN
+    EXEC('
+    CREATE FUNCTION [$attribute.capsule].[r$attribute.name] (
+        @changingTimepoint $attribute.timeRange
+    )
+    RETURNS TABLE WITH SCHEMABINDING AS RETURN
+    SELECT
+        $(schema.METADATA)? $attribute.metadataColumnName,
+        $attribute.anchorReferenceName,
+        $(attribute.hasChecksum())? $attribute.checksumColumnName,
+        $attribute.valueColumnName,
+        $attribute.changingColumnName
+    FROM
+        [$attribute.capsule].[$attribute.name]
+    WHERE
+        $attribute.changingColumnName <= @changingTimepoint;
+    ');
+END
+GO
+~*/
+            }
+            else {
+/*~
+-- Attribute rewinder -------------------------------------------------------------------------------------------------
+-- r$attribute.name rewinding over changing time function
+-----------------------------------------------------------------------------------------------------------------------
+IF Object_ID('r$attribute.name','IF') IS NULL
+BEGIN
+    EXEC('
+    CREATE FUNCTION [$attribute.capsule].[r$attribute.name] (
+        @changingTimepoint $attribute.timeRange
+    )
+    RETURNS TABLE WITH SCHEMABINDING AS RETURN
+    SELECT
+        $(schema.METADATA)? $attribute.metadataColumnName,
+        $attribute.anchorReferenceName,
+        $attribute.knotReferenceName,
+        $attribute.changingColumnName
+    FROM
+        [$attribute.capsule].[$attribute.name]
+    WHERE
+        $attribute.changingColumnName <= @changingTimepoint;
+    ');
+END
+GO
+~*/
+            }
+        }
+    }
+}
