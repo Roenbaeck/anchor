9a91856963eb2f9d9bf8f79068514a53099a5ebe
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2014-04-02 10:04:24 +0000

    Replaced Oracle naming convention with the updated one from SQL Server, which hopefully fixes the non-unique identity columns. Pushing to test.

--- SQL/Oracle/NamingConvention.js
+++ SQL/Oracle/NamingConvention.js
@@ -3,6 +3,10 @@
 // delimiter that is used in the naming convention
 var D = '_';
 
+// used in business naming
+var businessIdentity = 'Id';
+var businessName;
+
 // set some hard coded defaults if they are missing
 schema.metadata.defaultCapsule = schema.metadata.defaultCapsule || 'dbo';
 schema.metadata.chronon = schema.metadata.chronon || 'datetime';
@@ -10,31 +14,57 @@ schema.metadata.chronon = schema.metadata.chronon || 'datetime';
 var knot;
 while (knot = schema.nextKnot()) {
     knot.name = knot.mnemonic + D + knot.descriptor;
+    knot.identityName = knot.name + D + schema.metadata.identitySuffix;
+    knot.equivalentName = knot.name + D + schema.metadata.equivalentSuffix;
+    knot.businessName = knot.descriptor;
     knot.valueColumnName = knot.name;
     knot.identityColumnName = knot.mnemonic + D + schema.metadata.identitySuffix;
+    knot.checksumColumnName = knot.mnemonic + D + schema.metadata.checksumSuffix;
+    knot.equivalentColumnName = knot.mnemonic + D + schema.metadata.equivalentSuffix;
     knot.capsule = knot.metadata.capsule || schema.metadata.defaultCapsule;
     knot.metadataColumnName = schema.metadata.metadataPrefix + D + knot.mnemonic;
+    knot.toString = function() { return this.mnemonic; };
+}
+
+// warn about naming clashes for business names
+if(schema.BUSINESS_VIEWS) {
+    var knots = {};
+    while (knot = schema.nextKnot()) {
+        if(!knots[knot.businessName])
+            knots[knot.businessName] = [knot];
+        else
+            knots[knot.businessName].push(knot);
+    }
+    for(businessName in knots) {
+        if(knots[businessName].length > 1)
+            alert(
+                    "WARNING: The knots " + knots[businessName].join(' and ') +
+                    " all have the same business name: " + businessName + "."
+            );
+    }
 }
 
 var anchor;
 while (anchor = schema.nextAnchor()) {
     anchor.name = anchor.mnemonic + D + anchor.descriptor;
+    anchor.businessName = anchor.descriptor;
     anchor.identityColumnName = anchor.mnemonic + D + schema.metadata.identitySuffix;
     anchor.capsule = anchor.metadata.capsule || schema.metadata.defaultCapsule;
     anchor.metadataColumnName = schema.metadata.metadataPrefix + D + anchor.mnemonic;
     anchor.dummyColumnName = anchor.mnemonic + D + schema.metadata.dummySuffix;
+    anchor.businessIdentityColumnName = anchor.descriptor + D + businessIdentity;
+    anchor.toString = function() { return this.mnemonic; };
     var attribute;
     while (attribute = anchor.nextAttribute()) {
-        attribute.uniqueMnemonic = attribute.mnemonic;
-//        attribute.uniqueMnemonic = anchor.mnemonic + D + attribute.mnemonic;
-        attribute.tablename = anchor.name + D + "Attr";
-        anchor.attributeTableName = attribute.tablename;
-        attribute.name = attribute.uniqueMnemonic + D + attribute.descriptor;
-//        attribute.name = attribute.uniqueMnemonic + D + anchor.descriptor + D + attribute.descriptor;
+        attribute.uniqueMnemonic = anchor.mnemonic + D + attribute.mnemonic;
+        attribute.name = attribute.uniqueMnemonic + D + anchor.descriptor + D + attribute.descriptor;
+        attribute.businessName = attribute.descriptor;
         attribute.positName = attribute.name + D + schema.metadata.positSuffix;
         attribute.annexName = attribute.name + D + schema.metadata.annexSuffix;
+        attribute.checksumColumnName = attribute.uniqueMnemonic + D + schema.metadata.checksumSuffix;
         attribute.identityColumnName = attribute.uniqueMnemonic + D + schema.metadata.identitySuffix;
         attribute.metadataColumnName = schema.metadata.metadataPrefix + D + attribute.uniqueMnemonic;
+        attribute.equivalentColumnName = attribute.uniqueMnemonic + D + schema.metadata.equivalentSuffix;
         attribute.versionColumnName = attribute.uniqueMnemonic + D + schema.metadata.versionSuffix;
         attribute.positingColumnName = attribute.uniqueMnemonic + D + schema.metadata.positingSuffix;
         attribute.positorColumnName = attribute.uniqueMnemonic + D + schema.metadata.positorSuffix;
@@ -42,13 +72,14 @@ while (anchor = schema.nextAnchor()) {
         attribute.reliableColumnName = attribute.uniqueMnemonic + D + schema.metadata.reliableSuffix;
         attribute.statementTypeColumnName = attribute.uniqueMnemonic + D + schema.metadata.statementTypeSuffix;
         if(schema.IMPROVED) {
-            attribute.anchorReferenceName = anchor.identityColumnName;
-//            attribute.anchorReferenceName = attribute.uniqueMnemonic + D + anchor.mnemonic + D + schema.metadata.identitySuffix;
+            attribute.anchorReferenceName = attribute.uniqueMnemonic + D + anchor.mnemonic + D + schema.metadata.identitySuffix;
             if(attribute.isKnotted()) {
                 knot = attribute.knot;
                 attribute.knotReferenceName = attribute.uniqueMnemonic + D + attribute.knotRange + D + schema.metadata.identitySuffix;
                 attribute.knotValueColumnName = attribute.uniqueMnemonic + D + knot.name;
+                attribute.knotChecksumColumnName = attribute.uniqueMnemonic + D + knot.checksumColumnName;
                 attribute.knotMetadataColumnName = attribute.uniqueMnemonic + D + knot.metadataColumnName;
+                attribute.knotBusinessName = attribute.businessName + D + knot.businessName;
             }
         }
         else {
@@ -66,15 +97,57 @@ while (anchor = schema.nextAnchor()) {
             attribute.changingColumnName = attribute.uniqueMnemonic + D + schema.metadata.changingSuffix;
         }
         attribute.capsule = attribute.metadata.capsule || schema.metadata.defaultCapsule;
+        attribute.toString = function() { return this.mnemonic; };
+    }
+}
+
+// warn about naming clashes for business names
+if(schema.BUSINESS_VIEWS) {
+    var anchors = {};
+    while (anchor = schema.nextAnchor()) {
+        if(!anchors[anchor.businessName])
+            anchors[anchor.businessName] = [anchor];
+        else
+            anchors[anchor.businessName].push(anchor);
+        var attributes = {};
+        while (attribute = anchor.nextAttribute()) {
+            if(!attributes[attribute.businessName])
+                attributes[attribute.businessName] = [attribute];
+            else
+                attributes[attribute.businessName].push(attribute);
+        }
+        for(businessName in attributes) {
+            if(attributes[businessName].length > 1)
+                alert(
+                        "WARNING: The attributes " + attributes[businessName].join(' and ') +
+                        " on the anchor " + anchor.name +
+                        " all have the same business name: " + businessName + "."
+                );
+        }
+    }
+    for(businessName in anchors) {
+        if(anchors[businessName].length > 1)
+            alert(
+                    "WARNING: The anchors " + anchors[businessName].join(' and ') +
+                    " all have the same business name: " + businessName + "."
+            );
     }
 }
 
 var tie;
 while (tie = schema.nextTie()) {
-    var name = '';
+    var name = '', bName = '';
     var role;
     while (role = tie.nextRole()) {
         role.name = role.type + D + role.role;
+        if(role.knot) {
+            role.businessName = role.role + D + role.knot.descriptor;
+            role.businessColumnName = role.businessName + D + businessIdentity;
+        }
+        else {
+            role.businessName = role.anchor.descriptor + D + role.role;
+            role.businessColumnName = role.businessName + D + businessIdentity;
+        }
         role.columnName = role.type + D + schema.metadata.identitySuffix + D + role.role;
         if(role.knot) {
             knot = role.knot;
@@ -88,13 +161,17 @@ while (tie = schema.nextTie()) {
             }
         }
         name += role.name;
-        if(tie.hasMoreRoles()) name += D;
+        bName += role.businessName;
+        if(tie.hasMoreRoles()) {
+            name += D;
+            bName += D;
+        }
     }
     tie.name = name;
+    tie.businessName = bName;
     tie.positName = tie.name + D + schema.metadata.positSuffix;
     tie.annexName = tie.name + D + schema.metadata.annexSuffix;
     tie.identityColumnName = tie.name + D + schema.metadata.identitySuffix;
-    tie.identity = 'bigint'; // TODO: remove hard coded value
     tie.positingColumnName = tie.name + D + schema.metadata.positingSuffix;
     tie.positorColumnName = tie.name + D + schema.metadata.positorSuffix;
     tie.reliabilityColumnName = tie.name + D + schema.metadata.reliabilitySuffix;
@@ -106,46 +183,23 @@ while (tie = schema.nextTie()) {
     if(tie.timeRange) {
         tie.changingColumnName = tie.name + D + schema.metadata.changingSuffix;
     }
+    tie.toString = function() { return tie.name; };
 }
 
-// This code dispatches relations and knots from ties to a an anchors instead of an actuall TIE
-// Loop throug all anchors and look in the xml tie if its dependent on some other objets described as ROLEs or TIEs
-while (anchor = schema.nextAnchor()) {
-    var tie;
+// warn about naming clashes for business names
+if(schema.BUSINESS_VIEWS) {
+    var ties = {};
     while (tie = schema.nextTie()) {
-        var role;
-        var currentAnchorTable = anchor.name, exportAttr, exportAttrFromTable, exportAttrToTable, anchorRoleFather, anchorRoleFatherId, knotRoleFather, childTable, knotRoleFatherType, i = 0;
-        while (role = tie.nextRole()) {
-            // anchorRole always comes before knorRole. Last achorRole will be set as father for all comming anchorKnots
-            if (!role.knot){ // anchorRole
-                i++;
-                if (1 == i){ // first anchorRole will be set as father to the rest of the anchorRoles
-                    anchorRoleFather = role.anchor.name;
-                    anchorRoleFatherId = role.anchor.identityColumnName;
-                } else if (anchor.mnemonic == role.type){ // does this anchor belong to someone
-                    childTable = role.anchor.name;
-                    if (anchorRoleFather == childTable){ // Exception for "Self join/join by prior"
-                        role.exportAttr = (anchorRoleFatherId + "_parent").substr(0,30);
-                    } else { // Ordinary forein key constraint 
-                        role.exportAttr = anchorRoleFatherId;
-                    }
-                    role.exportAttrFromTable = anchorRoleFather;
-                    role.exportAttrToTable = childTable;
-                    role.exportRelation = "FOREIN KEY";
-                }
-                knotRoleFather = role.anchor.name;
-                knotRoleFatherType = role.type; // matches anchor.mnemonic
-            } else { // knotRole
-                // If there is knotRole check if previus anchorRole matches current currentAnchorTable
-                //if (knotRoleFather == currentAnchorTable){
-                if (anchor.mnemonic == knotRoleFatherType){
-                    role.exportAttr = role.type + D + schema.metadata.identitySuffix;
-                    role.exportAttrFromTable = tie.name;
-                    role.exportAttrToTable = currentAnchorTable;
-                    role.exportRelation = "KNOT";
-                }
-            }
-        }
+        if(!ties[tie.businessName])
+            ties[tie.businessName] = [tie];
+        else
+            ties[knot.businessName].push(tie);
+    }
+    for(businessName in ties) {
+        if(ties[businessName].length > 1)
+            alert(
+                    "WARNING: The ties " + ties[businessName].join(' and ') +
+                    " all have the same business name: " + businessName + "."
+            );
     }
 }
-
