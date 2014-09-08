c1c9cb6b3a0dd5323b4084cde831b52a823455f9
lars.ronnback@gmail.com <lars.ronnback@gmail.com@bfa991c8-53a5-3350-3af9-35fd0b1b703c>
2014-09-08 14:33:17 +0000

    Fixed bug when using other encapsulation than 'dbo' with business views. Pushing to test.

--- trunk/SQL/SQLServer/biz/DropTieBusinessPerspectives.js
+++ trunk/SQL/SQLServer/biz/DropTieBusinessPerspectives.js
@@ -10,24 +10,24 @@ if(schema.BUSINESS_VIEWS) {
 ~*/
         if(schema.EQUIVALENCE) {
 /*~
-IF Object_ID('EQ_Difference_$tie.businessName', 'IF') IS NOT NULL
+IF Object_ID('$tie.capsule$.EQ_Difference_$tie.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$tie.capsule].[EQ_Difference_$tie.businessName];
-IF Object_ID('EQ_Current_$tie.businessName', 'IF') IS NOT NULL
+IF Object_ID('$tie.capsule$.EQ_Current_$tie.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$tie.capsule].[EQ_Current_$tie.businessName];
-IF Object_ID('EQ_Point_$tie.businessName', 'IF') IS NOT NULL
+IF Object_ID('$tie.capsule$.EQ_Point_$tie.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$tie.capsule].[EQ_Point_$tie.businessName];
-IF Object_ID('EQ_Latest_$tie.businessName', 'IF') IS NOT NULL
+IF Object_ID('$tie.capsule$.EQ_Latest_$tie.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$tie.capsule].[EQ_Latest_$tie.businessName];
 ~*/
         }
 /*~
-IF Object_ID('Difference_$tie.businessName', 'IF') IS NOT NULL
+IF Object_ID('$tie.capsule$.Difference_$tie.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$tie.capsule].[Difference_$tie.businessName];
-IF Object_ID('Current_$tie.businessName', 'V') IS NOT NULL
+IF Object_ID('$tie.capsule$.Current_$tie.businessName', 'V') IS NOT NULL
 DROP VIEW [$tie.capsule].[Current_$tie.businessName];
-IF Object_ID('Point_$tie.businessName', 'IF') IS NOT NULL
+IF Object_ID('$tie.capsule$.Point_$tie.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$tie.capsule].[Point_$tie.businessName];
-IF Object_ID('Latest_$tie.businessName', 'V') IS NOT NULL
+IF Object_ID('$tie.capsule$.Latest_$tie.businessName', 'V') IS NOT NULL
 DROP VIEW [$tie.capsule].[Latest_$tie.businessName];
 GO
 ~*/
