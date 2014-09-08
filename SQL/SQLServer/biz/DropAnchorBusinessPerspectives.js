b813274d197a90ece9563500ba19a434f400abfd
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2014-09-08 14:33:17 +0000

    Fixed bug when using other encapsulation than 'dbo' with business views. Pushing to test.

--- SQL/SQLServer/biz/DropAnchorBusinessPerspectives.js
+++ SQL/SQLServer/biz/DropAnchorBusinessPerspectives.js
@@ -10,24 +10,24 @@ if(schema.BUSINESS_VIEWS) {
 ~*/
         if(schema.EQUIVALENCE) {
 /*~
-IF Object_ID('EQ_Difference_$anchor.businessName', 'IF') IS NOT NULL
+IF Object_ID('$anchor.capsule$.EQ_Difference_$anchor.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$anchor.capsule].[EQ_Difference_$anchor.businessName];
-IF Object_ID('EQ_Current_$anchor.businessName', 'IF') IS NOT NULL
+IF Object_ID('$anchor.capsule$.EQ_Current_$anchor.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$anchor.capsule].[EQ_Current_$anchor.businessName];
-IF Object_ID('EQ_Point_$anchor.businessName', 'IF') IS NOT NULL
+IF Object_ID('$anchor.capsule$.EQ_Point_$anchor.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$anchor.capsule].[EQ_Point_$anchor.businessName];
-IF Object_ID('EQ_Latest_$anchor.businessName', 'IF') IS NOT NULL
+IF Object_ID('$anchor.capsule$.EQ_Latest_$anchor.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$anchor.capsule].[EQ_Latest_$anchor.businessName];
 ~*/
         }
 /*~
-IF Object_ID('Difference_$anchor.businessName', 'IF') IS NOT NULL
+IF Object_ID('$anchor.capsule$.Difference_$anchor.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$anchor.capsule].[Difference_$anchor.businessName];
-IF Object_ID('Current_$anchor.businessName', 'V') IS NOT NULL
+IF Object_ID('$anchor.capsule$.Current_$anchor.businessName', 'V') IS NOT NULL
 DROP VIEW [$anchor.capsule].[Current_$anchor.businessName];
-IF Object_ID('Point_$anchor.businessName', 'IF') IS NOT NULL
+IF Object_ID('$anchor.capsule$.Point_$anchor.businessName', 'IF') IS NOT NULL
 DROP FUNCTION [$anchor.capsule].[Point_$anchor.businessName];
-IF Object_ID('Latest_$anchor.businessName', 'V') IS NOT NULL
+IF Object_ID('$anchor.capsule$.Latest_$anchor.businessName', 'V') IS NOT NULL
 DROP VIEW [$anchor.capsule].[Latest_$anchor.businessName];
 GO
 ~*/
