c8320859e7e77b9287f8dde9b63caecbaae029b4
elari-ttu <elari.ttu@gmail.com>
2014-12-18 15:13:31 +0200

    Descriptions
    Translated descriptions.

--- SQL/PostgreSQL/uni/AddDescriptions.js
+++ SQL/PostgreSQL/uni/AddDescriptions.js
@@ -1,45 +1,31 @@
 /*~
 -- DESCRIPTIONS -------------------------------------------------------------------------------------------------------
 ~*/
+
 var knot;
+
 while (knot = schema.nextKnot()) {
     if(knot.description &&
        knot.description._description &&
        knot.description._description.length > 0) {
 /*~
-BEGIN TRY
-EXEC sp_dropextendedproperty
-@name = N'MS_Description',
-@level0type = N'Schema', @level0name = '$knot.capsule',
-@level1type = N'Table',  @level1name = $(knot.isEquivalent())? '$knot.identityName'; : '$knot.name';
-END TRY BEGIN CATCH BEGIN TRY ROLLBACK END TRY BEGIN CATCH END CATCH END CATCH -- workaround for MS bug 658556
-EXEC sp_addextendedproperty
-@name = N'MS_Description',
-@value = '$knot.description._description',
-@level0type = N'Schema', @level0name = '$knot.capsule',
-@level1type = N'Table',  @level1name = $(knot.isEquivalent())? '$knot.identityName'; : '$knot.name';
-GO
+COMMENT ON TABLE 
+$(knot.isEquivalent())? $knot.identityName : $knot.name
+IS '$knot.description._description';
 ~*/
     }
 }
+
 var anchor;
+
 while (anchor = schema.nextAnchor()) {
     if(anchor.description &&
        anchor.description._description &&
        anchor.description._description.length > 0) {
 /*~
-BEGIN TRY
-EXEC sp_dropextendedproperty
-@name = N'MS_Description',
-@level0type = N'Schema', @level0name = '$anchor.capsule',
-@level1type = N'Table',  @level1name = '$anchor.name';
-END TRY BEGIN CATCH BEGIN TRY ROLLBACK END TRY BEGIN CATCH END CATCH END CATCH -- workaround for MS bug 658556
-EXEC sp_addextendedproperty
-@name = N'MS_Description',
-@value = '$anchor.description._description',
-@level0type = N'Schema', @level0name = '$anchor.capsule',
-@level1type = N'Table',  @level1name = '$anchor.name';
-GO
+COMMENT ON TABLE 
+$anchor.name
+IS '$anchor.description._description';
 ~*/
     }
     var attribute;
@@ -48,40 +34,24 @@ GO
            attribute.description._description &&
            attribute.description._description.length > 0) {
 /*~
-BEGIN TRY
-EXEC sp_dropextendedproperty
-@name = N'MS_Description',
-@level0type = N'Schema', @level0name = '$attribute.capsule',
-@level1type = N'Table',  @level1name = '$attribute.name';
-END TRY BEGIN CATCH BEGIN TRY ROLLBACK END TRY BEGIN CATCH END CATCH END CATCH -- workaround for MS bug 658556
-EXEC sp_addextendedproperty
-@name = N'MS_Description',
-@value = '$attribute.description._description',
-@level0type = N'Schema', @level0name = '$attribute.capsule',
-@level1type = N'Table',  @level1name = '$attribute.name';
-GO
+COMMENT ON TABLE 
+$attribute.name
+IS '$attribute.description._description';
 ~*/
         }
     }
 }
+
 var tie;
+
 while (tie = schema.nextTie()) {
     if(tie.description &&
        tie.description._description &&
        tie.description._description.length > 0) {
 /*~
-BEGIN TRY
-EXEC sp_dropextendedproperty
-@name = N'MS_Description',
-@level0type = N'Schema', @level0name = '$tie.capsule',
-@level1type = N'Table',  @level1name = '$tie.name';
-END TRY BEGIN CATCH BEGIN TRY ROLLBACK END TRY BEGIN CATCH END CATCH END CATCH -- workaround for MS bug 658556
-EXEC sp_addextendedproperty
-@name = N'MS_Description',
-@value = '$tie.description._description',
-@level0type = N'Schema', @level0name = '$tie.capsule',
-@level1type = N'Table',  @level1name = '$tie.name';
-GO
+COMMENT ON TABLE 
+$tie.name
+IS '$tie.description._description';
 ~*/
     }
     var role;
@@ -90,20 +60,9 @@ GO
            role.description._description &&
            role.description._description.length > 0) {
 /*~
-BEGIN TRY
-EXEC sp_dropextendedproperty
-@name = N'MS_Description',
-@level0type = N'Schema', @level0name = '$tie.capsule',
-@level1type = N'Table',  @level1name = '$tie.name',
-@level2type = N'Column', @level2name = '$role.columnName';
-END TRY BEGIN CATCH BEGIN TRY ROLLBACK END TRY BEGIN CATCH END CATCH END CATCH -- workaround for MS bug 658556
-EXEC sp_addextendedproperty
-@name = N'MS_Description',
-@value = '$role.description._description',
-@level0type = N'Schema', @level0name = '$tie.capsule',
-@level1type = N'Table',  @level1name = '$tie.name',
-@level2type = N'Column', @level2name = '$role.columnName';
-GO
+COMMENT ON COLUMN 
+$tie.name\.$role.columnName
+IS '$role.description._description';
 ~*/
         }
     }
