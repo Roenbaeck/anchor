307d8ffe9e227d08e86ab9c413829e7bf5b2bb04
elari-ttu <elari.ttu@gmail.com>
2015-03-02 10:47:54 +0200

    Finalised anchors and attributes.
    Added capsules and DROP-commands.

--- SQL/PostgreSQL/uni/CreateAnchorsAndAttributes.js
+++ SQL/PostgreSQL/uni/CreateAnchorsAndAttributes.js
@@ -20,7 +20,9 @@ while (anchor = schema.nextAnchor()) {
 -- Anchor table -------------------------------------------------------------------------------------------------------
 -- $anchor.name table (with ${(anchor.attributes ? anchor.attributes.length : 0)}$ attributes)
 -----------------------------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS _$anchor.name (
+-- DROP TABLE IF EXISTS $anchor.capsule\._$anchor.name;
+
+CREATE TABLE IF NOT EXISTS $anchor.capsule\._$anchor.name (
     $anchor.identityColumnName $(anchor.isGenerator())? $anchor.identityGenerator not null, : $anchor.identity not null,
     $(schema.METADATA)? $anchor.metadataColumnName $schema.metadata.metadataType not null, : $anchor.dummyColumnName boolean null,
     constraint pk$anchor.name primary key (
@@ -28,9 +30,15 @@ CREATE TABLE IF NOT EXISTS _$anchor.name (
     )
 );
 
-ALTER TABLE IF EXISTS ONLY _$anchor.name CLUSTER ON pk$anchor.name;
+ALTER TABLE IF EXISTS ONLY $anchor.capsule\._$anchor.name CLUSTER ON pk$anchor.name;
 
-CREATE OR REPLACE VIEW $anchor.name AS SELECT * FROM _$anchor.name;
+-- DROP VIEW IF EXISTS $anchor.capsule\.$anchor.name;
+
+CREATE OR REPLACE VIEW $anchor.capsule\.$anchor.name AS SELECT 
+    $anchor.identityColumnName,
+    $(schema.METADATA)? $anchor.metadataColumnName : $anchor.dummyColumnName
+FROM $anchor.capsule\._$anchor.name;
+
 ~*/
     var knot, attribute;
     while (attribute = anchor.nextAttribute()) {
@@ -40,7 +48,9 @@ CREATE OR REPLACE VIEW $anchor.name AS SELECT * FROM _$anchor.name;
 -- Historized attribute table -----------------------------------------------------------------------------------------
 -- $attribute.name table (on $anchor.name)
 -----------------------------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS _$attribute.name (
+-- DROP TABLE IF EXISTS $attribute.capsule\._$attribute.name;
+
+CREATE TABLE IF NOT EXISTS $attribute.capsule\._$attribute.name (
     $attribute.anchorReferenceName $anchor.identity not null,
     $(attribute.isEquivalent())? $attribute.equivalentColumnName $schema.metadata.equivalentRange not null,
     $attribute.valueColumnName $attribute.dataRange not null,
@@ -49,17 +59,27 @@ CREATE TABLE IF NOT EXISTS _$attribute.name (
     $(schema.METADATA)? $attribute.metadataColumnName $schema.metadata.metadataType not null,
     constraint fk$attribute.name foreign key (
         $attribute.anchorReferenceName
-    ) references _$anchor.name($anchor.identityColumnName),
+    ) references $anchor.capsule\._$anchor.name($anchor.identityColumnName),
     constraint pk$attribute.name primary key (
         $(attribute.isEquivalent())? $attribute.equivalentColumnName,
         $attribute.anchorReferenceName,
         $attribute.changingColumnName
     )
-)$(attribute.isEquivalent())? $scheme; : ;
+);
 
-ALTER TABLE IF EXISTS ONLY _$attribute.name CLUSTER ON pk$attribute.name;
+ALTER TABLE IF EXISTS ONLY $attribute.capsule\._$attribute.name CLUSTER ON pk$attribute.name;
 
-CREATE OR REPLACE VIEW $attribute.name AS SELECT * FROM _$attribute.name;
+-- DROP VIEW IF EXISTS $attribute.capsule\.$attribute.name;
+
+CREATE OR REPLACE VIEW $attribute.capsule\.$attribute.name AS SELECT
+    $attribute.anchorReferenceName,
+    $(attribute.isEquivalent())? $attribute.equivalentColumnName,
+    $attribute.valueColumnName,
+    $(attribute.hasChecksum())? $attribute.checksumColumnName,
+    $attribute.changingColumnName$(schema.METADATA)?,
+    $(schema.METADATA)? $attribute.metadataColumnName
+FROM $attribute.capsule\._$attribute.name;
+
 ~*/
     }
     else if(attribute.isHistorized() && attribute.isKnotted()) {
@@ -69,26 +89,36 @@ CREATE OR REPLACE VIEW $attribute.name AS SELECT * FROM _$attribute.name;
 -- Knotted historized attribute table ---------------------------------------------------------------------------------
 -- $attribute.name table (on $anchor.name)
 -----------------------------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS _$attribute.name (
+-- DROP TABLE IF EXISTS $attribute.capsule\._$attribute.name;
+
+CREATE TABLE IF NOT EXISTS $attribute.capsule\._$attribute.name (
     $attribute.anchorReferenceName $anchor.identity not null,
     $attribute.knotReferenceName $knot.identity not null,
     $attribute.changingColumnName $attribute.timeRange not null,
     $(schema.METADATA)? $attribute.metadataColumnName $schema.metadata.metadataType not null,
     constraint fk_A_$attribute.name foreign key (
         $attribute.anchorReferenceName
-    ) references _$anchor.name($anchor.identityColumnName),
+    ) references $anchor.capsule\._$anchor.name($anchor.identityColumnName),
     constraint fk_K_$attribute.name foreign key (
         $attribute.knotReferenceName
-    ) references _$knotTableName($knot.identityColumnName),
+    ) references $knot.capsule\._$knotTableName($knot.identityColumnName),
     constraint pk$attribute.name primary key (
         $attribute.anchorReferenceName,
         $attribute.changingColumnName
     )
 );
 
-ALTER TABLE IF EXISTS ONLY _$attribute.name CLUSTER ON pk$attribute.name;
+ALTER TABLE IF EXISTS ONLY $attribute.capsule\._$attribute.name CLUSTER ON pk$attribute.name;
 
-CREATE OR REPLACE VIEW $attribute.name AS SELECT * FROM _$attribute.name;
+-- DROP VIEW IF EXISTS $attribute.capsule\.$attribute.name;
+
+CREATE OR REPLACE VIEW $attribute.capsule\.$attribute.name AS SELECT
+    $attribute.anchorReferenceName,
+    $attribute.knotReferenceName,
+    $attribute.changingColumnName$(schema.METADATA)?,
+    $(schema.METADATA)? $attribute.metadataColumnName
+FROM $attribute.capsule\._$attribute.name;
+
 ~*/
     }
     else if(attribute.isKnotted()) {
@@ -99,24 +129,33 @@ CREATE OR REPLACE VIEW $attribute.name AS SELECT * FROM _$attribute.name;
 -- Knotted static attribute table -------------------------------------------------------------------------------------
 -- $attribute.name table (on $anchor.name)
 -----------------------------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS _$attribute.name (
+-- DROP TABLE IF EXISTS $attribute.capsule\._$attribute.name;
+
+CREATE TABLE IF NOT EXISTS $attribute.capsule\._$attribute.name (
     $attribute.anchorReferenceName $anchor.identity not null,
     $attribute.knotReferenceName $knot.identity not null,
     $(schema.METADATA)? $attribute.metadataColumnName $schema.metadata.metadataType not null,
     constraint fk_A_$attribute.name foreign key (
         $attribute.anchorReferenceName
-    ) references _$anchor.name($anchor.identityColumnName),
+    ) references $anchor.capsule\._$anchor.name($anchor.identityColumnName),
     constraint fk_K_$attribute.name foreign key (
         $attribute.knotReferenceName
-    ) references _$knotTableName($knot.identityColumnName),
+    ) references $knot.capsule\._$knotTableName($knot.identityColumnName),
     constraint pk$attribute.name primary key (
         $attribute.anchorReferenceName
     )
 );
 
-ALTER TABLE IF EXISTS ONLY _$attribute.name CLUSTER ON pk$attribute.name;
+ALTER TABLE IF EXISTS ONLY $attribute.capsule\._$attribute.name CLUSTER ON pk$attribute.name;
 
-CREATE OR REPLACE VIEW $attribute.name AS SELECT * FROM _$attribute.name;
+-- DROP VIEW IF EXISTS $attribute.capsule\.$attribute.name;
+
+CREATE OR REPLACE VIEW $attribute.capsule\.$attribute.name AS SELECT
+    $attribute.anchorReferenceName,
+    $attribute.knotReferenceName$(schema.METADATA)?,
+    $(schema.METADATA)? $attribute.metadataColumnName
+FROM $attribute.capsule\._$attribute.name;
+
 ~*/
     }
     else {
@@ -124,7 +163,9 @@ CREATE OR REPLACE VIEW $attribute.name AS SELECT * FROM _$attribute.name;
 -- Static attribute table ---------------------------------------------------------------------------------------------
 -- $attribute.name table (on $anchor.name)
 -----------------------------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS _$attribute.name (
+-- DROP TABLE IF EXISTS $attribute.capsule\._$attribute.name;
+
+CREATE TABLE IF NOT EXISTS $attribute.capsule\._$attribute.name (
     $attribute.anchorReferenceName $anchor.identity not null,
     $(attribute.isEquivalent())? $attribute.equivalentColumnName $schema.metadata.equivalentRange not null,
     $attribute.valueColumnName $attribute.dataRange not null,
@@ -132,16 +173,25 @@ CREATE TABLE IF NOT EXISTS _$attribute.name (
     $(schema.METADATA)? $attribute.metadataColumnName $schema.metadata.metadataType not null,
     constraint fk$attribute.name foreign key (
         $attribute.anchorReferenceName
-    ) references _$anchor.name($anchor.identityColumnName),
+    ) references $anchor.capsule\._$anchor.name($anchor.identityColumnName),
     constraint pk$attribute.name primary key (
         $(attribute.isEquivalent())? $attribute.equivalentColumnName,
         $attribute.anchorReferenceName
     )
-)$(attribute.isEquivalent())? $scheme; : ;
+);
 
-ALTER TABLE IF EXISTS ONLY _$attribute.name CLUSTER ON pk$attribute.name;
+ALTER TABLE IF EXISTS ONLY $attribute.capsule\._$attribute.name CLUSTER ON pk$attribute.name;
 
-CREATE OR REPLACE VIEW $attribute.name AS SELECT * FROM _$attribute.name;
+-- DROP VIEW IF EXISTS $attribute.capsule\.$attribute.name;
+
+CREATE OR REPLACE VIEW $attribute.capsule\.$attribute.name AS SELECT
+    $attribute.anchorReferenceName,
+    $(attribute.isEquivalent())? $attribute.equivalentColumnName,
+    $attribute.valueColumnName$(attribute.hasChecksum() || schema.METADATA)?,
+    $(attribute.hasChecksum())? $attribute.checksumColumnName~*//*~$(attribute.hasChecksum() && schema.METADATA)?,
+    $(schema.METADATA)? $attribute.metadataColumnName
+FROM $attribute.capsule\._$attribute.name;
+
 ~*/
     }
   }
