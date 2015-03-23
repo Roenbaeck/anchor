4abdff46555e6e50b89eb207e6260665fa31397c
elari-ttu <elari.ttu@gmail.com>
2015-03-23 11:14:54 +0200

    Finalized anchor difference perspective.

--- SQL/PostgreSQL/uni/CreateAnchorPerspectiveDifference.js
+++ SQL/PostgreSQL/uni/CreateAnchorPerspectiveDifference.js
@@ -8,7 +8,7 @@ while (anchor = schema.nextAnchor()) {
 -- Difference perspective ---------------------------------------------------------------------------------------------
 -- d$anchor.name showing all differences between the given timepoints and optionally for a subset of attributes
 -----------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION d$anchor.name (
+CREATE OR REPLACE FUNCTION $anchor.capsule\.d$anchor.name (
     intervalStart $schema.metadata.chronon,
     intervalEnd $schema.metadata.chronon,
     selection text = null
@@ -55,7 +55,7 @@ FROM (
         CAST($attribute.changingColumnName AS $schema.metadata.chronon) AS inspectedTimepoint,
         ''$attribute.mnemonic'' AS mnemonic
     FROM
-        $(attribute.isEquivalent())? e$attribute.name(0) : $attribute.name
+        $(attribute.isEquivalent())? $attribute.capsule\.e$attribute.name(0) : $attribute.capsule\.$attribute.name
     WHERE
         (selection is null OR selection like ''%$attribute.mnemonic%'')
     AND
@@ -66,7 +66,7 @@ FROM (
 /*~
 ) timepoints
 CROSS JOIN LATERAL
-    p$anchor.name(timepoints.inspectedTimepoint) p$anchor.mnemonic
+    $anchor.capsule\.p$anchor.name(timepoints.inspectedTimepoint) p$anchor.mnemonic
 WHERE
     p$anchor.mnemonic\.$anchor.identityColumnName = timepoints.$anchor.identityColumnName;
 ' LANGUAGE SQL;
