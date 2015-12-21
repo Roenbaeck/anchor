149cba815a5ffa58b7816a524564b61121ad65ab
Christian Fischer <sw-dev@computerlyrik.de>
2015-12-21 21:28:06 +0100

    added callback to sisulator to get all his directives and files

--- Sisulator.js
+++ Sisulator.js
@@ -52,60 +52,59 @@ var Sisulator = {
         // just initialize and return the result
         return objectifier(xml.documentElement, map, {});
     },
-    sisulate: function(xml, directive) {
+    sisulate: function(xml, map, directives) {
         // objectify the xml
         var schema = Sisulator.objectify(xml, Sisulator.MAP).schema;
         var cacheDisabler = '';
         if(DEBUG) cacheDisabler = '?r=' + Math.random();
         // this variable holds the result
         var _sisula_ = '';
-
-        // process and evaluate all sisulas in the directive
-        var xmlhttp = new window.XMLHttpRequest();
-        xmlhttp.open("GET", directive + cacheDisabler, false);
-        xmlhttp.send(null);
-        var response = xmlhttp.responseText.replace(/\r/g, ''); // unify line breaks
+        var scripts = directives();
+        scripts = scripts.replace(/\r/g, ''); // unify line breaks
         // only non-empty lines that are not comments (starts with #)
-        var scripts = response.match(/^[^#].+/gm);
-        var script;
-        var splitter = /\/\*~([\s\S]*?)~\*\//g; // split JS /*~ sisula template ~*/ JS
-        // process every sisula
-        for(var s = 0; script = scripts[s]; s++) {
-            xmlhttp.open("GET", script.trim() + cacheDisabler, false);
-            xmlhttp.send(null);
-            var sisula = xmlhttp.responseText;
-            // split language into JavaScript and SQL template components
-            var sisulets = sisula.split(splitter);
-            // substitute from SQL template to JavaScript
-            for(var i = 1; i < sisulets.length; i+=2) {
-                // honor escaped dollar signs
-                sisulets[i] = sisulets[i].replace(/[$]{2}/g, '§DOLLAR§'); // escaping dollar signs
-                sisulets[i] = sisulets[i].replace(/["]{2}/g, '§QUOTED§'); // escaping double quotes
-                sisulets[i] = sisulets[i].replace(/[$]{([\S\s]*?)}[$]/g, '" + $1 + "'); // multi-expression
-                sisulets[i] = sisulets[i].replace(/[$]\(([\S\s]*?)\)\?[^\S\n]*([^:\n]*)[:]?[^\S\n]*(.*)/g, '" + ($1 ? "$2" : "$3") + "'); // conditional
-                sisulets[i] = sisulets[i].replace(/[\$]([\w.]*?)(?:([\$])|([^\w.]|$))/g, '" + ($1 ? $1 : "") + "$3'); // single
-                sisulets[i] = sisulets[i].replace(/(\r\n|\n|\r)/g, '\\n" +\n'); // line breaks
-                sisulets[i] = sisulets[i].replace(/^/gm, '"'); // start of line
-                sisulets[i] = '_sisula_+=' + sisulets[i] + '";'; // variable assignment
+        scripts = scripts.match(/^[^#].+/gm);
+        if(scripts) {
+            var script;
+            var splitter = /\/\*~|~\*\//g; // split JS /*~ sisula template ~*/ JS
+            // process every sisula
+            for(var scriptIndex = 0; script = scripts[scriptIndex]; scriptIndex++) {
+                script = script.replace(/^\s+/,'').replace(/\s+$/,''); // trim
+                var sisula = directives(script);
+                // make sure everything starts with JavaScript (empty row)
+                sisula = '\n' + sisula;
+                // split language into JavaScript and SQL template components
+                var sisulets = sisula.split(splitter);
+                // substitute from SQL template to JavaScript
+                for(var i = 1; i < sisulets.length; i+=2) {
+                    // honor escaped dollar signs
+                    sisulets[i] = sisulets[i].replace(/[$]{2}/g, '§DOLLAR§'); // escaping dollar signs
+                    sisulets[i] = sisulets[i].replace(/["]{2}/g, '§DOUBLE§'); // escaping double quotes
+                    sisulets[i] = sisulets[i].replace(/["]{1}/g, '§SINGLE§'); // escaping single quotes
+                    sisulets[i] = sisulets[i].replace(/[$]{([\S\s]*?)}[$]/g, '" + $1 + "'); // multi-expression
+                    sisulets[i] = sisulets[i].replace(/[$]\(([\S\s]*?)\)\?[^\S\n]*([^:\n]*)[:]?[^\S\n]*(.*)/g, '" + ($1 ? "$2" : "$3") + "'); // conditional
+                    sisulets[i] = sisulets[i].replace(/[\$]([\w.]*?)(?:([\$])|([^\w.]|$))/g, '" + ($1 ? $1 : "") + "$3'); // single
+                    sisulets[i] = sisulets[i].replace(/(\r\n|\n|\r)/g, '\\n" +\n'); // line breaks
+                    sisulets[i] = sisulets[i].replace(/^/gm, '"'); // start of line
+                    sisulets[i] = '_sisula_+=' + sisulets[i] + '";'; // variable assignment
+                }
+                // join the parts together again (now all JavaScript)
+                sisula = sisulets.join('');
+                try {
+                    // this eval needs schema and _sisula_ to be defined
+                    eval(sisula);
+                }
+                catch(e) {
+                    alert('Error in script: ' + script);
+                    alert(sisula); // alert was used for debugging sisula code
+                    throw e;
+                }
             }
-            // join the parts together again (now all JavaScript)
-            sisula = sisulets.join('');
-            try {
-                if(DEBUG && console && console.log)
-                    console.log(sisula);
-                // this eval needs schema and _sisula_ to be defined
-                eval(sisula);
-            }
-            catch(e) {
-                if(DEBUG && console && console.error && script)
-                    console.error('The script ' + script + ' could not be executed.');
-                throw e;
-            }
+            _sisula_ = _sisula_.replace(/§DOLLAR§/g, '$'); // unescaping dollar signs
+            _sisula_ = _sisula_.replace(/§SINGLE§/g, '\"'); // unescaping double quotes
+            _sisula_ = _sisula_.replace(/§QUOTED§/g, '"'); // unescaping double quotes
+            _sisula_ = _sisula_.replace(/^\s*[\r\n]/gm, ''); // remove empty lines
+            _sisula_ = _sisula_.replace(/(\S+[^\S\n])(?:[^\S\n]+)/gm, '$1'); // consume multiple spaces, but not indentation
         }
-        _sisula_ = _sisula_.replace(/§DOLLAR§/g, '$'); // unescaping dollar signs
-        _sisula_ = _sisula_.replace(/§QUOTED§/g, '"'); // unescaping double quotes
-        _sisula_ = _sisula_.replace(/^\s*[\r\n]/gm, ''); // remove empty lines
-        _sisula_ = _sisula_.replace(/(\S+[^\S\n])(?:[^\S\n]+)/gm, '$1'); // consume multiple spaces, but not indentation
         return _sisula_;
     }
 };
