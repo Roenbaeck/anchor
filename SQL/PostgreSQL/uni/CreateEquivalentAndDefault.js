bc3148617bfeb290bd2bf950651abf6f4791d39a
DataDenken <datadenken@MacBook-Pro-van-DataDenken.local>
2020-04-23 22:10:45 +0200

    Added CreateEquivalentAndDefault

--- SQL/PostgreSQL/uni/CreateEquivalentAndDefault.js
+++ SQL/PostgreSQL/uni/CreateEquivalentAndDefault.js
@@ -6,21 +6,17 @@ if(schema.EQUIVALENCE) {
 -- must be available the table is set up with a default equivalent with identity 0.
 --
 -- Equivalent table ---------------------------------------------------------------------------------------------------
-IF Object_ID('$schema.metadata.encapsulation$._$schema.metadata.equivalentSuffix', 'U') IS NULL
-BEGIN
-    CREATE TABLE [$schema.metadata.encapsulation].[_$schema.metadata.equivalentSuffix] (
-        $schema.metadata.equivalentSuffix $schema.metadata.equivalentRange not null,
-        constraint pk_$schema.metadata.equivalentSuffix primary key (
-            $schema.metadata.equivalentSuffix asc
-        )
-    );
-    INSERT INTO [$schema.metadata.encapsulation].[_$schema.metadata.equivalentSuffix] (
+CREATE TABLE IF NOT EXISTS $schema.metadata.encapsulation\._$schema.metadata.equivalentSuffix (
+    $schema.metadata.equivalentSuffix $schema.metadata.equivalentRange not null,
+    constraint pk_$schema.metadata.equivalentSuffix primary key (
         $schema.metadata.equivalentSuffix
     )
-    VALUES (
-        0 -- the default equivalent
-    );
-END
-GO
+);
+
+INSERT INTO $schema.metadata.encapsulation\._$schema.metadata.equivalentSuffix (
+    $schema.metadata.equivalentSuffix
+) VALUES (
+    0 -- the default equivalent
+);
 ~*/
 }
