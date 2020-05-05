cecf03db0d9f2ab752ba58c961a63284d5151a52
Juan Jose van der Linden <delostilos@mac.com>
2020-05-05 15:42:43 +0200

    Added latest and now perspective to attributes

--- SQL/PostgreSQL/uni/CreateAttributePerspectives.js
+++ SQL/PostgreSQL/uni/CreateAttributePerspectives.js
@@ -1,12 +1,20 @@
 /*~
--- ATTRIBUTE REWINDERS ------------------------------------------------------------------------------------------------
+-- ATTRIBUTE TEMPORAL PERSPECTIVES ---------------------------------------------------------------------------------------
 --
--- These table valued functions rewind an attribute table to the given
--- point in changing time. It does not pick a temporal perspective and
--- instead shows all rows that have been in effect before that point
--- in time.
+-- These table valued functions simplify temporal querying by providing a temporal
+-- perspective of each attribute. There are three types of perspectives: latest,
+-- point-in-time and now. 
 --
--- @changingTimepoint   the point in changing time to rewind to
+-- The latest perspective shows the latest available information for each attribute.
+-- The now perspective shows the information as it is right now.
+-- The point-in-time perspective lets you travel through the information to the given timepoint.
+--
+-- @changingTimepoint   the point in changing time to travel to
+--
+-- Under equivalence all these views default to equivalent = 0, however, corresponding
+-- prepended-e perspectives are provided in order to select a specific equivalent.
+--
+-- @equivalent          the equivalent for which to retrieve data
 --
 ~*/
 var anchor;
@@ -23,35 +31,63 @@ while (anchor = schema.nextAnchor()) {
                 attributeValueColumnType = attribute.dataRange;
             }            
 /*~
--- Attribute Point-in-time perspective -------------------------------------------------------------------------------------------------
--- p$attribute.name iewed as it was on the given timepoint
+-- Latest perspective -------------------------------------------------------------------------------------------------
+-- l$attribute.name viewed by the latest available information (may include future versions)
 -----------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION $attribute.capsule\.p$attribute.name(
-    $(attribute.isEquivalent())? equivalent $schema.metadata.equivalentRange,
-    changingTimepoint $attribute.timeRange
-) RETURNS TABLE(
-    $(schema.METADATA)? $attribute.metadataColumnName $schema.metadata.metadataType,
-    $attribute.anchorReferenceName $anchor.identity,
-    $(attribute.isEquivalent() && !attribute.isKnotted())? $attribute.equivalentColumnName $schema.metadata.equivalentRange,
-    $(!attribute.isKnotted() && attribute.hasChecksum())? $attribute.checksumColumnName bytea,
-    $attribute.valueColumnName $attributeValueColumnType,
-    $attribute.changingColumnName $attribute.timeRange
-) AS '
-    SELECT DISTINCT ON ($attribute.anchorReferenceName) $attribute.anchorReferenceName,
-        $(schema.METADATA)? $attribute.metadataColumnName,
-        $(attribute.isEquivalent() && !attribute.isKnotted())? $attribute.equivalentColumnName,
-        $(!attribute.isKnotted() && attribute.hasChecksum())? $attribute.checksumColumnName,
-        $attribute.valueColumnName,
-        $attribute.changingColumnName
-    FROM
-        $(attribute.isEquivalent() && !attribute.isKnotted())? $attribute.capsule\.e$attribute.name(equivalent) : $attribute.capsule\.$attribute.name
-    WHERE
-        $attribute.changingColumnName <= changingTimepoint
-    ORDER BY
-        $attribute.anchorReferenceName DESC,
-        $attribute.changingColumnName DESC     
-    ;
-' LANGUAGE SQL STABLE;
+
+CREATE OR REPLACE VIEW $attribute.capsule\.l$attribute.name AS
+SELECT DISTINCT ON ($attribute.anchorReferenceName) $attribute.anchorReferenceName
+     , $attribute.valueColumnName
+     , $attribute.changingColumnName
+     $(schema.METADATA)? , $attribute.metadataColumnName
+     $(attribute.isEquivalent() && !attribute.isKnotted())? , $attribute.equivalentColumnName
+     $(!attribute.isKnotted() && attribute.hasChecksum())? , $attribute.checksumColumnName
+  FROM $(attribute.isEquivalent() && !attribute.isKnotted())? $attribute.capsule\.e$attribute.name(equivalent) : $attribute.capsule\.$attribute.name
+ ORDER 
+    BY $attribute.anchorReferenceName DESC
+     , $attribute.changingColumnName DESC   
+;
+
+-- Attribute Point-in-time perspective -------------------------------------------------------------------------------------------------
+-- p$attribute.name viewed as it was on the given timepoint
+-----------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION $attribute.capsule\.p$attribute.name
+( changingTimepoint $attribute.timeRange
+$(attribute.isEquivalent())? , equivalent $schema.metadata.equivalentRange
+) 
+RETURNS TABLE
+( $attribute.anchorReferenceName $anchor.identity
+, $attribute.valueColumnName $attributeValueColumnType
+, $attribute.changingColumnName $attribute.timeRange    
+$(schema.METADATA)? , $attribute.metadataColumnName $schema.metadata.metadataType
+$(attribute.isEquivalent() && !attribute.isKnotted())? , $attribute.equivalentColumnName $schema.metadata.equivalentRange
+$(!attribute.isKnotted() && attribute.hasChecksum())? , $attribute.checksumColumnName bytea
+) 
+AS 
+'
+SELECT DISTINCT ON ($attribute.anchorReferenceName) $attribute.anchorReferenceName
+     , $attribute.valueColumnName
+     , $attribute.changingColumnName
+     $(schema.METADATA)? , $attribute.metadataColumnName
+     $(attribute.isEquivalent() && !attribute.isKnotted())? , $attribute.equivalentColumnName
+     $(!attribute.isKnotted() && attribute.hasChecksum())? , $attribute.checksumColumnName
+  FROM $(attribute.isEquivalent() && !attribute.isKnotted())? $attribute.capsule\.e$attribute.name(equivalent) : $attribute.capsule\.$attribute.name
+ WHERE $attribute.changingColumnName <= changingTimepoint
+ ORDER 
+    BY $attribute.anchorReferenceName DESC
+     , $attribute.changingColumnName DESC     
+;
+' 
+LANGUAGE SQL STABLE
+;
+
+-- Now perspective ----------------------------------------------------------------------------------------------------
+-- n$attribute.name viewed as it currently is (cannot include future versions)
+-----------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW $attribute.capsule\.n$attribute.name AS
+SELECT *
+  FROM $attribute.capsule\.p$attribute.name($schema.metadata.now::$attribute.timeRange);
+;
 ~*/
         }
     }
